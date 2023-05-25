38bef2668a46f7849a50f1308ca7c15994d74b02
ruizii <santiago-rb1@outlook.es>
2023-05-25 02:01:58 -0500

    cmdheight=0

--- options.lua
+++ options.lua
@@ -8,7 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     scrolloff = 0,
-    cmdheight = 1,
+    cmdheight = 0,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
