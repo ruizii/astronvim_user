0beb247ff9998900ec3957060332985fc1432904
ruizii <santiago-rb1@outlook.es>
2023-07-21 15:04:28 -0500

    Added languages for mason ensure installed

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,11 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "pyright",
+        "clangd",
+        "bashls",
+        "gopls",
+        "tsserver"
       })
     end,
   },
@@ -18,8 +22,11 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "autopep8",
+        "gofumpt",
+        "beautysh",
+        "clang-format"
       })
     end,
   },
@@ -29,7 +36,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
+        "delve",
+        "bash",
+        "cppdbg"
       })
     end,
   },
