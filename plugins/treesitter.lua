063c8cd3573b86d8f15b61a31a5695c3bba92331
ruizii <santiago-rb1@outlook.es>
2023-07-21 14:20:25 -0500

    Added languages for treesitter ensure installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,15 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "python",
+      "c",
+      "cpp",
+      "php",
+      "bash",
+      "go",
+      "javascript",
+      "typescript"
     })
   end,
 }
