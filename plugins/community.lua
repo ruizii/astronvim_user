68941ba5d62a833a508d919661fcd3a318ccd8f4
ruizii <95191116+ruizii@users.noreply.github.com>
2023-09-29 22:36:21 -0500

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,5 +7,7 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-  { import = "astrocommunity.lsp.lsp-signature-nvim" }
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cpp" }
 }
