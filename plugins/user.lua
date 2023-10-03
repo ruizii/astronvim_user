9480f5d2d45d9ce4bd348d201d5e2f08eeccd955
ruizii <95191116+ruizii@users.noreply.github.com>
2023-10-03 17:12:29 -0500

    Update user.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     "CRAG666/code_runner.nvim",
     lazy = false,
@@ -53,45 +52,4 @@ return {
       vim.notify = require "notify"
     end,
   },
-
-  {
-    -- override nvim-cmp plugin
-    "hrsh7th/nvim-cmp",
-    -- override the options table that is used in the `require("cmp").setup()` call
-    opts = function(_, opts)
-      -- opts parameter is the default options table
-      -- the function is lazy loaded so cmp is able to be required
-      local cmp = require "cmp"
-      -- modify the mapping part of the table
-
-      opts.mapping["<Tab>"] = cmp.mapping.confirm {
-        select = true,
-      }
-      opts.mapping["<CR>"] = cmp.config.disable
-      opts.mapping["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select }
-      opts.mapping["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }
-
-      cmp.setup {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-      }
-
-      -- opts.mapping["<Up>"] = cmp.mapping.select_prev_item(),
-
-      -- return the new table to be used
-      return opts
-    end,
-  },
-
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
 }
